6d48729ac783bb3e64a7352480717ecd733e6258
Abel Haukur <abelcat9@hotmail.com>
2023-02-21 10:53:38 +0000

    Code cleanup and refactoring

--- src/test/java/is/hi/hbv202g/ass6/CarOwnerTest.java
+++ src/test/java/is/hi/hbv202g/ass6/CarOwnerTest.java
@@ -1,16 +1,26 @@
 package is.hi.hbv202g.ass6;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CarOwnerTest {
 
+
+    public static final String DOE = "John Doe";
+    private Car car;
+
+    @Before
+    public void setUp() throws Exception {
+        car = new Car("Beetle");
+    }
+
     @Test
     public void getName() {
-        Car car=new Car("Beetle");
-        CarOwner carOwner = new CarOwner("John Doe", car);
-        assertEquals("John Doe", carOwner.getName());
+        CarOwner carOwner = new CarOwner(DOE, car);
+        assertEquals(DOE, carOwner.getName());
     }
 
 }
